c5a38f2488cd9636bd3569fc59611e0bb8a3fc1c
Charlie Hornsby <chornsby@aiven.io>
2021-01-29 17:24:11 +0200

    Prevent child process disconnecting parent connection to Redis
    When a new task is started it is forked to run in its own process (to
    protect the coordinator process from crashing if something goes wrong
    in a task). In the child process the task reconnects to Redis (to avoid
    sharing the connection with the parent process) but this reconnect is
    implemented as a disconnect followed by a connect.
    
    The disconnect here is not appropriate because the parent process might
    be currently using that connection. When connecting to Redis using SSL
    (which is becoming more common now Redis itself supports TLS natively)
    this matters and causes the following error:
    
    SSL_read: sslv3 alert bad record mac (OpenSSL::SSL::SSLError)
    
    By calling "connect" here rather than "reconnect" we leave the parent
    connection to Redis intact and create a new one for child processes if
    they run with fork_per_job = true.

--- lib/resque/data_store.rb
+++ lib/resque/data_store.rb
@@ -76,7 +76,8 @@ module Resque
       @redis.inspect
     end
 
-    # Force a reconnect to Redis.
+    # Force a reconnect to Redis without closing the connection in the parent
+    # process after a fork.
     def reconnect
       @redis._client.connect
     end
